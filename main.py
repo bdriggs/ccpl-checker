61487ace2249dc48be37a179cb43eaa70a6f63d2
Britton Riggs <bdriggs01@gmail.com>
2025-01-18 09:13:58 -0700

    Refactored

--- main.py
+++ main.py
@@ -1,22 +1,49 @@
-from utilities import ccpl
+import pandas as pd
+import requests
+import json
+from datetime import datetime
 
-csv_url = 'https://discover.cuyahogalibrary.org/Search/Results?lookfor=PS5&searchIndex=Keyword&filter[]=format%3A%22Video+Game%22&filter[]=availability_toggle%3A%22available%22&sort=relevance&view=excel&searchSource=local'
-available_games_path = './files/available_games.json'
-new_games_path = './files/new_games.json'
-csv_path = './files/SearchResults.csv'
+csv_url = "https://discover.cuyahogalibrary.org/Search/Results?lookfor=&searchIndex=Keyword&filter[]=format%3A%22PlayStation+5%22&filter[]=availability_toggle%3A%22available%22&sort=relevance&view=excel&searchSource=local"
+csv_path = "SearchResults.csv"
+
+
+def download_csv_from_url(url, output_filename):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open(output_filename, "wb") as file:
+                file.write(response.content)
+        else:
+            print(f"Failed to download CSV. Status code: {response.status_code}")
+    except requests.RequestException as e:
+        print(f"Error downloading CSV: {e}")
+
 
 def main():
-    ccpl.download_csv_from_url(csv_url, csv_path)
-    games_csv = ccpl.read_csv_file(csv_path)
+    download_csv_from_url(csv_url, csv_path)
 
-    available_games = ccpl.get_available_games(games_csv)
-    previous_available_games = ccpl.read_previous_available_games(
-        available_games_path)
-    new_games = ccpl.compare_games(previous_available_games, available_games)
+    csv_file = 'SearchResults.csv'  
+    previous_json_file = 'previous_titles.json'
+    output_json_file = f'new_titles_{datetime.now().strftime("%Y%m%d")}.json'
 
-    ccpl.remove_file(csv_path)
-    ccpl.write_json(available_games_path, available_games)
-    ccpl.write_json(new_games_path, new_games)
+    current_data = pd.read_csv(csv_file)
+    current_games = {row['Title']: row['Link'] for _, row in current_data[['Title', 'Link']].dropna().iterrows()}
+
+    try:
+        with open(previous_json_file, 'r') as file:
+            previous_games = json.load(file)  
+    except FileNotFoundError:
+        previous_games = {}
+
+    new_games = {title: link for title, link in current_games.items() if title not in previous_games}
+
+    with open(output_json_file, 'w') as file:
+        json.dump(new_games, file, indent=4)
+
+    with open(previous_json_file, 'w') as file:
+        json.dump(current_games, file, indent=4)
+
+    print(f"New games saved to: {output_json_file}")
 
 
 if __name__ == "__main__":
